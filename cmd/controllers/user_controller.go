5e7524f7fe524eaeb52d4f575e94d99e0852ae89
alexpiczenik <alexpiczenik@gmail.com>
2023-06-15 19:14:59 -0300

    Bcrypt fix

--- cmd/controllers/user_controller.go
+++ cmd/controllers/user_controller.go
@@ -18,7 +18,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"golang.org/x/crypto/bcrypt"
 )
 
 var userCollection *mongo.Collection = configs.GetCollection(configs.DB, "users")
